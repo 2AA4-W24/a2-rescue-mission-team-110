03eba9252ed3e6c5c5ef40312fa72d2021df453c
CalvinSalsali04 <c.salsali1@gmail.com>
2024-02-04 23:08:22 -0500

    Battery level class

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Battery.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.se2aa4.island.team110;
+
+public class Battery{
+  private int batteryLevel;
+
+  public Battery (int batteryLevel){
+    this.batteryLevel = totalbatterylevel;
+  }
+
+  public void batteryConsumption(){
+  }
+
+}
