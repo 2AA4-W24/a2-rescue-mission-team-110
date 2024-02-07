68e9ab0005a8c84fec0fd111f3ae7302c46615d6
richardhan0530@gmail.com <richardhan0530@gmail.com>
2024-02-07 15:51:46 -0500

    created UAV interface

--- src/main/java/ca/mcmaster/se2aa4/island/team110/Interfaces/UAV.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team110/Interfaces/UAV.java
@@ -1,2 +1,15 @@
+package ca.mcmaster.se2aa4.island.team110.Interfaces;
 
+public interface UAV{
+
+    int getBattery();
+
+    int getPosX();
+
+    int getPostY();
+
+    String getDirection();
+
+
+}
 
