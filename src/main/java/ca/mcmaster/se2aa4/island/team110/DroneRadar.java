08b5081956533a1da22351e36ff78010822b28e5
richardhan0530@gmail.com <richardhan0530@gmail.com>
2024-02-07 16:25:56 -0500

    created radar interface

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team110/DroneRadar.java
@@ -0,0 +1,7 @@
+package ca.mcmaster.se2aa4.island.team110;
+
+import ca.mcmaster.se2aa4.island.team110.Interfaces.Radar;
+
+public class DroneRadar {
+    
+}
--- src/main/java/ca/mcmaster/se2aa4/island/team110/Interfaces/Radar.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team110/Interfaces/Radar.java
@@ -1,5 +1,10 @@
 package ca.mcmaster.se2aa4.island.team110.Interfaces;
+import ca.mcmaster.se2aa4.island.team110.Enums.RadarResponse;
+
 
 public interface Radar {
 
+    RadarResponse sendEcho();
+
+    RadarResponse receiveEcho();
 }
