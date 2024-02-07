40ce5065182d6122e3e27e94c1734e85a1df0c5e
Raymond Tao <taor10@mcmaster.ca>
2024-02-07 16:59:51 -0500

    Radar interface and droneRadar class skeleton

--- src/main/java/ca/mcmaster/se2aa4/island/team110/DroneRadar.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team110/DroneRadar.java
@@ -1,7 +1,39 @@
 package ca.mcmaster.se2aa4.island.team110;
 
 import ca.mcmaster.se2aa4.island.team110.Interfaces.Radar;
+import ca.mcmaster.se2aa4.island.team110.Enums.RadarResponse;
+import ca.mcmaster.se2aa4.island.team110.Enums.Directions;
 
-public class DroneRadar implements Radar {
-    
+public class DroneRadar implements Radar{
+
+    private int radarRange;
+
+    public DroneRadar(int radarRange) { 
+
+        this.radarRange = radarRange;
+
+    }
+
+    @Override
+    public RadarResponse sendEcho(Directions direction){
+
+      if (direction == Directions.NORTH){
+        return RadarResponse.GROUND; //sample example on how to implement the enum
+      }
+
+    }
+
+
+
+    public int getRadarRange(){
+      return radarRange;
+    }
+
+    public void setRadarRange(int radarRange){
+      this.radarRange = radarRange;
+
+    }
+
+
+
 }
--- src/main/java/ca/mcmaster/se2aa4/island/team110/Interfaces/Radar.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team110/Interfaces/Radar.java
@@ -1,10 +1,10 @@
 package ca.mcmaster.se2aa4.island.team110.Interfaces;
+import ca.mcmaster.se2aa4.island.team110.Enums.Directions;
 import ca.mcmaster.se2aa4.island.team110.Enums.RadarResponse;
-
 
 public interface Radar {
 
-    RadarResponse sendEcho();
+    RadarResponse sendEcho(Directions direction);
 
     RadarResponse receiveEcho();
 }
