c9ad1bd0e40bb758b2ad5f446c3c5b0e62877572
Raymond Tao <taor10@mcmaster.ca>
2024-02-16 02:42:26 -0500

    Skeleton for radar logic

--- src/main/java/ca/mcmaster/se2aa4/island/team110/Interfaces/Radar.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team110/Interfaces/Radar.java
@@ -4,7 +4,6 @@ import ca.mcmaster.se2aa4.island.team110.Enums.RadarResponse;
 
 public interface Radar {
 
-    RadarResponse sendEcho(Directions direction);
-
-    RadarResponse receiveEcho();
+    RadarResponse getEchoResponse(Directions direction);
+    
 }
